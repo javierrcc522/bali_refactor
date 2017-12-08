b46d2bc044480c37cbb2755fe7439a79d7a47cfc
Javier Jimenez <javierrcc522@gmail.com>
2017-12-08 15:28:14 -0800

    fix this helper method by adding a counter and adding each quantity of that item

--- app/helpers/cart_helper.rb
+++ app/helpers/cart_helper.rb
@@ -1,5 +1,9 @@
 module CartHelper
   def cart_items
-    current_order.order_items.length
+    total = 0
+    current_order.order_items.each do |order_item|
+      total += order_item.quantity
+    end
+    total
   end
 end
