c901a56ccebb78f27fd1afdee6bfdbd7b81b80d5
Javier Jimenez <javierrcc522@gmail.com>
2017-12-08 15:29:10 -0800

    changed the path to get it ready for AJAx

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -14,7 +14,7 @@ class OrderItemsController < ApplicationController
     if @order.save
       session[:order_id] = @order.id
       flash[:notice] = "This product has been added to your cart."
-      redirect_to cart_path
+      redirect_to products_path
     else
       flash[:notice] = "Please add items."
       redirect_to root_path
